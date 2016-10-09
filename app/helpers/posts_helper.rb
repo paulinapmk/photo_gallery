c4a3b19132dc94cf2d390c8a7b07d9e2e59868b8
Paulina <plnkania14@gmail.com>
2016-10-09 15:37:29 +0200

    like post helpers and 'real-time' update

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,26 +6,35 @@ module PostsHelper
     image_tag 'sign-in.jpg', id: 'image-preview', class: 'img-responsive'
   end
 
-  def likers_of(post)
-    # votes variable is set to the likes by users.
+  def display_likes(post)
     votes = post.votes_for.up.by_type(User)
-    # set user_names variable as an empty array
+    return list_likers(votes) if votes.size <= 8
+    count_likers(votes)
+  end
+
+  def liked_post(post)
+    return 'glyphicon-heart' if current_user.voted_for? post
+    'glyphicon-heart-empty'
+  end
+
+  private
+
+  def list_likers(votes)
     user_names = []
-    # unless there are no likes, continue below.
     unless votes.blank?
-      # iterate through the voters of each vote (the users who liked the post)
       votes.voters.each do |voter|
-        # add the user_name as a link to the array
         user_names.push(link_to voter.user_name,
                                 profile_path(voter.user_name),
                                 class: 'user-name')
       end
-      # present the array as a nice sentence using the as_sentence method and also make it usable within our html.  Then call the like_plural method with the votes variable we set earlier as the argument.
       user_names.to_sentence.html_safe + like_plural(votes)
     end
   end
 
-  private
+  def count_likers(votes)
+    vote_count = votes.size
+    vote_count.to_s + ' likes'
+  end
 
   def like_plural(votes)
     # If we more than one like for a post, use ' like this'
